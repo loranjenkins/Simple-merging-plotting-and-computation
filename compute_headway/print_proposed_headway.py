8df214e91da20d9ff8c97b5b3dc2d8a4a4f71dc7
Loran Bogaart <loranbogaart@outlook.com>
2022-07-13 18:17:35 +0200

    Optimized computations

--- compute_headway/print_proposed_headway.py
+++ compute_headway/print_proposed_headway.py
@@ -2,25 +2,30 @@ from compute_headway.conditiondefinition import ConditionDefinition
 import math
 
 
-# def _from_difference_at_point(left_velocity, right_velocity, left_headway, headway distance, name):
+# def _from_difference_at_point(left_velocity, right_velocity, left_headway, headway, distance, name):
 
 headway = ConditionDefinition._from_difference_at_point(50, 50, 0, 294, 'test')
+
+#geometry
+angle_from_merge_point = 0.102206718 #radians
+width = 30
+height = 292.5
 
 print('right_initial_offset:', headway.right_initial_position_offset)
 print('left_initial_offset:', headway.left_initial_position_offset)
 
 def ComputeX(theta, right_difference):
-    desired_x = 30-right_difference*math.sin(theta)
+    desired_x = width-right_difference*math.sin(theta)
     return desired_x
 
 def ComputeY(theta, right_difference):
-    desired_y = 292.5-right_difference*math.cos(theta)
+    desired_y = height-right_difference*math.cos(theta)
     return desired_y
 
-desired_x_right = ComputeX(0.102206718, headway.right_initial_position_offset)
-desired_y_right = ComputeY(0.102206718, headway.right_initial_position_offset)
-desired_x_left = ComputeX(0.102206718, headway.left_initial_position_offset)
-desired_y_left = ComputeY(0.102206718, headway.left_initial_position_offset)
+desired_x_right = ComputeX(angle_from_merge_point, headway.right_initial_position_offset)
+desired_y_right = ComputeY(angle_from_merge_point, headway.right_initial_position_offset)
+desired_x_left = ComputeX(angle_from_merge_point, headway.left_initial_position_offset)
+desired_y_left = ComputeY(angle_from_merge_point, headway.left_initial_position_offset)
 print("The x value is for right vehicle: ", desired_x_right)
 print("The y value is for right vehicle: ", desired_y_right)
 print("The x value is for left vehicle: ", desired_x_left)
