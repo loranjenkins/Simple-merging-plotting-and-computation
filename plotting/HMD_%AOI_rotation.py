65618cb0c09e597c60b75eeaa5d739a179ada188
Loran Bogaart <loranbogaart@outlook.com>
2022-09-23 15:36:55 +0200

    Update

--- plotting/HMD_%AOI_rotation.py
+++ plotting/HMD_%AOI_rotation.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
-import datetime
 from pathlib import Path
+from scipy.interpolate import make_interp_spline, BSpline
+from scipy.ndimage import gaussian_filter1d
 
 from trackobjects.simulationconstants import SimulationConstants
 from trackobjects.symmetricmerge import SymmetricMergingTrack
@@ -22,34 +22,7 @@ def vehicle_xy_coordinates(intcolumn, data_csv):
 
     return list_x, list_y
 
-
-
-# def plot_varjo_data(list_of_csv):
-#     simulation_constants = SimulationConstants(vehicle_width=2,
-#                                                vehicle_length=4.7,
-#                                                tunnel_length=118,
-#                                                track_width=8,
-#                                                track_height=195,
-#                                                track_start_point_distance=390,
-#                                                track_section_length_before=275.77164466275354,
-#                                                track_section_length_after=200)  # goes until 400
-#
-#     track = SymmetricMergingTrack(simulation_constants)
-#
-#     all_pds_list = []
-#     for i in range(len(list_of_csv)):
-#         all_pds = pd.read_csv(trails[i], sep=',')
-#         all_pds_list.append(all_pds)
-#
-#     xy_coordinates_for_trace = []
-#     for i in range(1):
-#         xy_coordinates = vehicle_xy_coordinates(2, all_pds_list[i])
-#         # xy_coordinates = [list(a) for a in zip(xy_coordinates_for_trace[0], xy_coordinates_for_trace[1])]
-#         # xy_coordinates_for_trace.append(xy_coordinates)
-#         print(xy_coordinates)
-
-
-if __name__ == '__main__':
+def plot_varjo(path_to_csv_folder):
     simulation_constants = SimulationConstants(vehicle_width=2,
                                                vehicle_length=4.7,
                                                tunnel_length=118,
@@ -60,7 +33,7 @@ if __name__ == '__main__':
                                                track_section_length_after=200)  # goes until 400
     track = SymmetricMergingTrack(simulation_constants)
 
-    files_directory = r'C:\Users\loran\Desktop\ExperimentOlgerArkady\Joan.Varjo.combined\Test varjo'
+    files_directory = path_to_csv_folder
     trails = []
     for file in Path(files_directory).glob('*.csv'):
         # trail_condition = plot_trail(file)
@@ -72,8 +45,6 @@ if __name__ == '__main__':
         all_pds = pd.read_csv(trails[i], sep=',')
         all_pds_list.append(all_pds)
 
-
-
     xy_coordinates_for_trace = []
     for i in range(len(all_pds_list)):
         xy_coordinates_vehicle = vehicle_xy_coordinates(2, all_pds_list[i])
@@ -81,7 +52,6 @@ if __name__ == '__main__':
         xy_coordinates_for_trace.append(xy_coordinates_zip)
 
     # print(xy_coordinates_for_trace[0])
-
 
     travelled_distance = []
     for list_index in range(len(xy_coordinates_for_trace)):
@@ -92,277 +62,70 @@ if __name__ == '__main__':
             inner_xy_list.append(traveled_distance_vehicle)
         travelled_distance.append(inner_xy_list)
 
+    indexes_of_tunnel_and_merge = []
+    for list_index in range(len(travelled_distance)):
+        individual_travelled_distance_vehicle = travelled_distance[list_index]
+        inner_tunnel_merge = []
+        for i in range(0,1):
+            index_of_tunnel_vehicle = min(range(len(individual_travelled_distance_vehicle)), key=lambda i: abs(individual_travelled_distance_vehicle[i] - track.tunnel_length))
+            index_of_mergepoint_vehicle = min(range(len(individual_travelled_distance_vehicle)), key=lambda i: abs(individual_travelled_distance_vehicle[i] - simulation_constants.track_section_length_before))
+            inner_tunnel_merge.append(index_of_tunnel_vehicle)
+            inner_tunnel_merge.append(index_of_mergepoint_vehicle)
+        indexes_of_tunnel_and_merge.append(inner_tunnel_merge)
+
+    interactive_area_travelled_traces = []
+    hmd_rot_interactive_area = []
+
+    for i in range(len(indexes_of_tunnel_and_merge)):
+        hmd_rot = list(all_pds_list[i]['HMD_rotation_vehicle1'][indexes_of_tunnel_and_merge[i][0]:indexes_of_tunnel_and_merge[i][1]])
+        interactive_trace = travelled_distance[i][indexes_of_tunnel_and_merge[i][0]:indexes_of_tunnel_and_merge[i][1]]
+        hmd_rot_interactive_area.append(hmd_rot)
+        interactive_area_travelled_traces.append(interactive_trace)
+
+    on_ramp_vs_opponent = []
+    for list_index in range(len(hmd_rot_interactive_area)):
+        individual_hmd_rot_list = hmd_rot_interactive_area[list_index]
+        inner_attention_list = []
+        for i in range(len(individual_hmd_rot_list)):
+            if individual_hmd_rot_list[i] > 0.99:
+                inner_attention_list.append(1)
+            else:
+                inner_attention_list.append(0)
+
+        on_ramp_vs_opponent.append(inner_attention_list)
+
+
+    df_traces = pd.DataFrame(interactive_area_travelled_traces)
+    x_mean_traces = df_traces.mean()
+    df_hmd_rotations = pd.DataFrame(on_ramp_vs_opponent)
+    y_mean_traces = df_hmd_rotations.mean()
+
+    fig, (ax1) = plt.subplots(1)
+    #
+    # ax1.title('Area of interest over travelled distance')
+    ax1.set_xlabel('average travelled distance [m]')
+    ax1.set_ylabel('% fixated on AOI')
+    # ax1.plot(x_mean_traces[0:500], y_mean_traces[0:500])
+    # print(len(x_mean_traces))
+    # print(len(y_mean_traces))
+
+    #find longest column and put in gaussian
+    print(df_traces)
+
+    ysmoothed = gaussian_filter1d(y_mean_traces, sigma=10)
+    ax1.plot(df_traces.iloc[5], ysmoothed)
+
+    # spl = make_interp_spline(x_mean_traces[0:750], y_mean_traces[0:750], k=3) #type: BSpline
+    # xnew = np.linspace(x_mean_traces[0:750].min(), x_mean_traces[0:750].max(), 100)
+    # power_smooth = spl(xnew)
+    # plt.plot(xnew, power_smooth)
+
+    plt.show()
 
 
 
-# if __name__ == '__main__':
-#     files_directory = r'C:\Users\loran\Desktop\ExperimentOlgerArkady\Joan.Varjo.combined\Test varjo'
-#     trails = []
-#     for file in Path(files_directory).glob('*.csv'):
-#         # trail_condition = plot_trail(file)
-#         trails.append(file)
-#
-#     simulation_constants = SimulationConstants(vehicle_width=2,
-#                                                vehicle_length=4.7,
-#                                                tunnel_length=118,
-#                                                track_width=8,
-#                                                track_height=195,
-#                                                track_start_point_distance=390,
-#                                                track_section_length_before=275.77164466275354,
-#                                                track_section_length_after=200)  # goes until 400
-#
-#     track = SymmetricMergingTrack(simulation_constants)
-#
-#     data = pd.read_csv(trails[0], sep=',')
-#     data1 = pd.read_csv(trails[1], sep=',')
-#
-#     xy_coordinates_vehicle1 = vehicle_xy_coordinates(2, data)
-#     xy_coordinates_vehicle1_trail1 = vehicle_xy_coordinates(2, data1)
-#
-#     xy_coordinates_vehicle1 = [list(a) for a in zip(xy_coordinates_vehicle1[0], xy_coordinates_vehicle1[1])]
-#     xy_coordinates_vehicle1_trail1 = [list(a) for a in zip(xy_coordinates_vehicle1_trail1[0], xy_coordinates_vehicle1_trail1[1])]
-#
-#     travelled_distance_vehicle1 = []
-#     travelled_distance_vehicle1_trail1 = []
-#     for i in range(len(xy_coordinates_vehicle1)):
-#         traveled_distance1 = track.coordinates_to_traveled_distance(xy_coordinates_vehicle1[i])
-#         traveled_distance1_trail1 = track.coordinates_to_traveled_distance(xy_coordinates_vehicle1_trail1[i])
-#         travelled_distance_vehicle1.append(traveled_distance1)
-#         travelled_distance_vehicle1_trail1.append(traveled_distance1_trail1)
-#
-#     index_of_tunnel_vehicle1 = min(range(len(travelled_distance_vehicle1)),
-#                                    key=lambda i: abs(travelled_distance_vehicle1[i] - track.tunnel_length))
-#
-#     index_of_mergepoint_vehicle1 = min(range(len(travelled_distance_vehicle1)),
-#                                    key=lambda i: abs(travelled_distance_vehicle1[i] - simulation_constants.track_section_length_before))
-#
-#     index_of_tunnel_vehicle1_trail1 = min(range(len(travelled_distance_vehicle1_trail1)),
-#                                    key=lambda i: abs(travelled_distance_vehicle1_trail1[i] - track.tunnel_length))
-#
-#     index_of_mergepoint_vehicle1_trail1 = min(range(len(travelled_distance_vehicle1_trail1)),
-#                                        key=lambda i: abs(travelled_distance_vehicle1_trail1[
-#                                                              i] - simulation_constants.track_section_length_before))
-#
-#     interactive_area_trace = travelled_distance_vehicle1[index_of_tunnel_vehicle1:index_of_mergepoint_vehicle1]
-#     interactive_area_trace_trail1 = travelled_distance_vehicle1_trail1[index_of_tunnel_vehicle1:index_of_mergepoint_vehicle1]
-#
-#     hmd_interactive_area = list(data['HMD_rotation_vehicle1'][index_of_tunnel_vehicle1:index_of_mergepoint_vehicle1])
-#     hmd_interactive_area_trail1 = list(data1['HMD_rotation_vehicle1'][index_of_tunnel_vehicle1_trail1:index_of_mergepoint_vehicle1_trail1])
-#
-#     interactive_dict = {'interactive_area_trace': [], 'hmd_interactive_area': []}
-#     interactive_dict_trail1 = {'interactive_area_trace': [], 'hmd_interactive_area': []}
-#
-#     for i in range(len(interactive_area_trace)):
-#         interactive_dict['interactive_area_trace'].append(interactive_area_trace[i])
-#         interactive_dict['hmd_interactive_area'].append(hmd_interactive_area[i])
-#
-#     for i in range(len(interactive_area_trace_trail1)):
-#         interactive_dict_trail1['interactive_area_trace'].append(interactive_area_trace_trail1[i])
-#         interactive_dict_trail1['hmd_interactive_area'].append(hmd_interactive_area_trail1[i])
-#
-#     df = pd.DataFrame(interactive_dict)
-#     df1 = pd.DataFrame(interactive_dict_trail1)
-#
-#     on_ramp_vs_opponent = []
-#     for i in range(len(df['hmd_interactive_area'])):
-#         if df['hmd_interactive_area'][i] > 0.98:
-#             on_ramp_vs_opponent.append(1)
-#         elif df['hmd_interactive_area'][i] < 0.98:
-#             on_ramp_vs_opponent.append(0)
-#
-#     on_ramp_vs_opponent_trail1 = []
-#     for i in range(len(df1['hmd_interactive_area'])):
-#         if df1['hmd_interactive_area'][i] > 0.98:
-#             on_ramp_vs_opponent_trail1.append(1)
-#         elif df1['hmd_interactive_area'][i] < 0.98:
-#             on_ramp_vs_opponent_trail1.append(0)
-#
-#     df['on_ramp_vs_opponent'] = on_ramp_vs_opponent
-#     df1['on_ramp_vs_opponent'] = on_ramp_vs_opponent_trail1
-#
-#     df_travelled_trace_combined = pd.concat([df['interactive_area_trace'], df1['interactive_area_trace']], axis=1, keys=['x1', 'x2'])
-#     df_travelled_trace_combined['mean'] = df_travelled_trace_combined.mean(axis=1)
-#
-#     df_fixation_combined = pd.concat([df['on_ramp_vs_opponent'], df1['on_ramp_vs_opponent']], axis=1, keys=['y1', 'y2'])
-#     df_fixation_combined['mean'] = df_fixation_combined.mean(axis=1)
-#
-#
-#     fig, (ax1) = plt.subplots(1)
-#     # fig.suptitle('-')
-#     # plt.title('positions over time')
-#     ax1.set_xlabel('average travelled distance [m]')
-#     ax1.set_ylabel('% fixated on AOI')
-#     # ax1.set_yticks([175, -5, 5, -175])
-#     # ax1.set_yticklabels([175, 0, 0, -175])
-#
-#
-#     # ax1.set_xlim(50, 450)
-#
-#     ax1.plot(df_travelled_trace_combined['mean'], df_fixation_combined['mean'])
-#
-#     plt.show()
+if __name__ == '__main__':
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#
-#
-#
-#
-#
-# import pandas as pd
-# import matplotlib.pyplot as plt
-# import numpy as np
-# import datetime
-# from pathlib import Path
-#
-# from trackobjects.simulationconstants import SimulationConstants
-# from trackobjects.symmetricmerge import SymmetricMergingTrack
-#
-# def vehicle_xy_coordinates(intcolumn, data_csv):
-#     list_x = []
-#     list_y = []
-#     for i in range(len(data_csv.iloc[:, intcolumn])):
-#         transform_vehicle1 = eval(data_csv.iloc[i, intcolumn])
-#         x_loc = transform_vehicle1[0]
-#         y_loc = transform_vehicle1[1]
-#
-#         list_x.append(x_loc)
-#         # list_x = list(dict.fromkeys(list_x))
-#         list_y.append(y_loc)
-#         # list_y = list(dict.fromkeys(list_y))
-#
-#     return list_x, list_y
-#
-# if __name__ == '__main__':
-#     files_directory = r'C:\Users\loran\Desktop\ExperimentOlgerArkady\Joan.Varjo.combined\Test varjo'
-#     trails = []
-#     for file in Path(files_directory).glob('*.csv'):
-#         # trail_condition = plot_trail(file)
-#         trails.append(file)
-#
-#     simulation_constants = SimulationConstants(vehicle_width=2,
-#                                                vehicle_length=4.7,
-#                                                tunnel_length=118,
-#                                                track_width=8,
-#                                                track_height=195,
-#                                                track_start_point_distance=390,
-#                                                track_section_length_before=275.77164466275354,
-#                                                track_section_length_after=200)  # goes until 400
-#
-#     track = SymmetricMergingTrack(simulation_constants)
-#
-#     data = pd.read_csv(trails[0], sep=',')
-#     data1 = pd.read_csv(trails[1], sep=',')
-#
-#     xy_coordinates_vehicle1 = vehicle_xy_coordinates(2, data)
-#     xy_coordinates_vehicle1_trail1 = vehicle_xy_coordinates(2, data1)
-#
-#     xy_coordinates_vehicle1 = [list(a) for a in zip(xy_coordinates_vehicle1[0], xy_coordinates_vehicle1[1])]
-#     xy_coordinates_vehicle1_trail1 = [list(a) for a in zip(xy_coordinates_vehicle1_trail1[0], xy_coordinates_vehicle1_trail1[1])]
-#
-#     travelled_distance_vehicle1 = []
-#     travelled_distance_vehicle1_trail1 = []
-#     for i in range(len(xy_coordinates_vehicle1)):
-#         traveled_distance1 = track.coordinates_to_traveled_distance(xy_coordinates_vehicle1[i])
-#         traveled_distance1_trail1 = track.coordinates_to_traveled_distance(xy_coordinates_vehicle1_trail1[i])
-#         travelled_distance_vehicle1.append(traveled_distance1)
-#         travelled_distance_vehicle1_trail1.append(traveled_distance1_trail1)
-#
-#     index_of_tunnel_vehicle1 = min(range(len(travelled_distance_vehicle1)),
-#                                    key=lambda i: abs(travelled_distance_vehicle1[i] - track.tunnel_length))
-#
-#     index_of_mergepoint_vehicle1 = min(range(len(travelled_distance_vehicle1)),
-#                                    key=lambda i: abs(travelled_distance_vehicle1[i] - simulation_constants.track_section_length_before))
-#
-#     index_of_tunnel_vehicle1_trail1 = min(range(len(travelled_distance_vehicle1_trail1)),
-#                                    key=lambda i: abs(travelled_distance_vehicle1_trail1[i] - track.tunnel_length))
-#
-#     index_of_mergepoint_vehicle1_trail1 = min(range(len(travelled_distance_vehicle1_trail1)),
-#                                        key=lambda i: abs(travelled_distance_vehicle1_trail1[
-#                                                              i] - simulation_constants.track_section_length_before))
-#
-#     interactive_area_trace = travelled_distance_vehicle1[index_of_tunnel_vehicle1:index_of_mergepoint_vehicle1]
-#     interactive_area_trace_trail1 = travelled_distance_vehicle1_trail1[index_of_tunnel_vehicle1:index_of_mergepoint_vehicle1]
-#
-#     hmd_interactive_area = list(data['HMD_rotation_vehicle1'][index_of_tunnel_vehicle1:index_of_mergepoint_vehicle1])
-#     hmd_interactive_area_trail1 = list(data1['HMD_rotation_vehicle1'][index_of_tunnel_vehicle1_trail1:index_of_mergepoint_vehicle1_trail1])
-#
-#     interactive_dict = {'interactive_area_trace': [], 'hmd_interactive_area': []}
-#     interactive_dict_trail1 = {'interactive_area_trace': [], 'hmd_interactive_area': []}
-#
-#     for i in range(len(interactive_area_trace)):
-#         interactive_dict['interactive_area_trace'].append(interactive_area_trace[i])
-#         interactive_dict['hmd_interactive_area'].append(hmd_interactive_area[i])
-#
-#     for i in range(len(interactive_area_trace_trail1)):
-#         interactive_dict_trail1['interactive_area_trace'].append(interactive_area_trace_trail1[i])
-#         interactive_dict_trail1['hmd_interactive_area'].append(hmd_interactive_area_trail1[i])
-#
-#     df = pd.DataFrame(interactive_dict)
-#     df1 = pd.DataFrame(interactive_dict_trail1)
-#
-#     on_ramp_vs_opponent = []
-#     for i in range(len(df['hmd_interactive_area'])):
-#         if df['hmd_interactive_area'][i] > 0.98:
-#             on_ramp_vs_opponent.append(1)
-#         elif df['hmd_interactive_area'][i] < 0.98:
-#             on_ramp_vs_opponent.append(0)
-#
-#     on_ramp_vs_opponent_trail1 = []
-#     for i in range(len(df1['hmd_interactive_area'])):
-#         if df1['hmd_interactive_area'][i] > 0.98:
-#             on_ramp_vs_opponent_trail1.append(1)
-#         elif df1['hmd_interactive_area'][i] < 0.98:
-#             on_ramp_vs_opponent_trail1.append(0)
-#
-#     df['on_ramp_vs_opponent'] = on_ramp_vs_opponent
-#     df1['on_ramp_vs_opponent'] = on_ramp_vs_opponent_trail1
-#
-#     df_travelled_trace_combined = pd.concat([df['interactive_area_trace'], df1['interactive_area_trace']], axis=1, keys=['x1', 'x2'])
-#     df_travelled_trace_combined['mean'] = df_travelled_trace_combined.mean(axis=1)
-#
-#     df_fixation_combined = pd.concat([df['on_ramp_vs_opponent'], df1['on_ramp_vs_opponent']], axis=1, keys=['y1', 'y2'])
-#     df_fixation_combined['mean'] = df_fixation_combined.mean(axis=1)
-#
-#
-#     fig, (ax1) = plt.subplots(1)
-#     # fig.suptitle('-')
-#     # plt.title('positions over time')
-#     ax1.set_xlabel('average travelled distance [m]')
-#     ax1.set_ylabel('% fixated on AOI')
-#     # ax1.set_yticks([175, -5, 5, -175])
-#     # ax1.set_yticklabels([175, 0, 0, -175])
-#
-#
-#     # ax1.set_xlim(50, 450)
-#
-#     ax1.plot(df_travelled_trace_combined['mean'], df_fixation_combined['mean'])
-#
-#     plt.show()
+    plot_varjo(r'C:\Users\loran\Desktop\ExperimentOlgerArkady\Joan.Varjo.combined')
 
 
